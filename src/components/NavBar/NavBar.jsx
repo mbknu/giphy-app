f83142c2059e669f477f410465f1f3109a1ec267
mbknu <bouchelot.m@gmail.com>
2020-11-18 15:59:09 +0100

    add searchbar not totaly working

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const NavBar = () => {
   return (
     <div className="nav-bar">
       <Link to="/">Home</Link>
+      <SearchBar />
       <Link to="/trending">Trending</Link>
       <Link to="/random">Random</Link>
       <Link to="/favorited">Favorites</Link>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { debounce } from "lodash";
 
-const SearchBar = () => {
-  return <div></div>;
+const SearchBar = (submitSearchValue) => {
+  const [searchValue, setSearchValue] = useState("");
+  console.log("searchValue", searchValue);
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Search Gifs..."
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+          const debouncedSbmit = debounce(
+            () => submitSearchValue(e.target.value),
+            1000
+          );
+          debouncedSbmit();
+        }}
+      ></input>
+    </div>
+  );
 };
 
 export default SearchBar;
